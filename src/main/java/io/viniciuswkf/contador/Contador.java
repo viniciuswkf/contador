42d8da825b92707e81a2fc39bb34db1f38357e6e
viniciuswkf <viniciusludv@gmail.com>
2023-08-24 08:08:19 -0300

    feat: contagem

---
+++ src/main/java/io/viniciuswkf/contador/Contador.java
@@ -0,0 +1,38 @@
+package io.viniciuswkf.contador;
+
+import java.util.Scanner;
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+        
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroDois <= parametroUm) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm + 1;  // Incluindo o segundo parâmetro na contagem
+
+        System.out.println("Contagem:");
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println(i);
+        }
+    }
+}
